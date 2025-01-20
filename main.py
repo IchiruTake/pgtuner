35bb01a663e13d3f79c29945edcf82ab82004ff1
Ichiru Take <66363520+IchiruTake@users.noreply.github.com>
2025-01-20 22:45:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import uvicorn
 import web
 
-if __name__ == "__main__":
-    uvicorn.run(web.app, host='0.0.0.0', port=8001, access_log=True, workers=1,
-                proxy_headers=False, server_header=False, date_header=False)
+uvicorn.run(web.app, host='0.0.0.0', port=8001, access_log=True, workers=1, proxy_headers=False, server_header=False, date_header=False)
